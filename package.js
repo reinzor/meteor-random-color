34fb093dc6748bec97fb153dfeb983449adda427
Krzysztof Rzymkowski <rzymek@gmail.com>
2014-10-21 15:08:23 +0200

    Update package.js
    allow randomColor on server

--- package.js
+++ package.js
@@ -4,6 +4,6 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('ui', 'client');
-    api.add_files('lib/js/randomColor.js', 'client');
+    api.add_files('lib/js/randomColor.js');
     api.add_files('client/helpers.js', 'client');
 });
